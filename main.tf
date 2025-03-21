6e1994476a2169786938ba5979928d5ed0e1c2b2
dipuvish5 <39113917+dipuvish5@users.noreply.github.com>
2025-03-21 10:59:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "my_instance" {
-  ami           = "ami-0abcdef1234567890"  # Change to your region's AMI
-  instance_type = "t2.micro"
+  ami           = "ami-0846b753e2af0da6e"  # Change to your region's AMI
+  instance_type = "t4g.nano"
   key_name      = "your-key-pair"          # Replace with your key pair
   tags = {
-    Name = "Scheduled-EC2"
+    Name = "Soln-Arch"
   }
 }
 
@@ -21,9 +21,9 @@ resource "aws_eip_association" "eip_assoc" {
 }
 
 resource "aws_route53_record" "my_record" {
-  zone_id = "Z123456789ABCDEFG"   # Replace with your Route 53 Hosted Zone ID
-  name    = "yourdomain.com"      # Replace with your domain
+  zone_id = "Z0358020WBTRB1CMQU2V"   # Replace with your Route 53 Hosted Zone ID
+  name    = "api-java.deepakvishwakarma.com"      # Replace with your domain
   type    = "A"
-  ttl     = 300
+  ttl     = 60
   records = [aws_eip.my_eip.public_ip]
 }
