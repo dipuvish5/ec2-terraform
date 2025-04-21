e1c617d4a201f7a1d1592ecc47ca4721d8c13084
dipuvish5 <39113917+dipuvish5@users.noreply.github.com>
2025-04-21 22:15:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "my_instance" {
   ami           = "ami-0846b753e2af0da6e"  # Change to your region's AMI
   instance_type = "t4g.nano"
   key_name      = "Terraform-On-Github"  # Only the key name is required
+  associate_public_ip_address = true  # THIS is required
   tags = {
     Name = "Soln-Arch"
   }
